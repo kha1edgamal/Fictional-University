fff2342cefdc3f346d7aedd9718a64f209f59c65
Khalid Jamal <5aled.gamal231@gmail.com>
2020-03-18 05:21:26 +0200

    Part 31

--- app/public/wp-content/themes/fractional-university-theme/front-page.php
+++ app/public/wp-content/themes/fractional-university-theme/front-page.php
@@ -17,26 +17,39 @@
 
         <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
         <?php
+          $today= date('Ymd');
           $homeEvents = new WP_Query(array(
-            'posts_per_page' => 2,
-            'post_type' => 'events'
+            'posts_per_page' => -1,
+            'post_type' => 'events',
+            'orderby' =>'meta_value_num',
+            'meta_key'=>'event_date',
+            'order'=>'ASC',
+            'meta_query'=>array(
+              array(
+                'key' => 'event_date',
+                'compare'=> '>=',
+                'value' => $today,
+                'type' => 'numeric'
+              )
+            )
+
           ));
           while($homeEvents->have_posts()) {
             $homeEvents->the_post(); ?>
         <div class="event-summary">
           <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
             <span class="event-summary__month"><?php 
-            $eventDate = new DateTime(get_field('event_date'));
-            echo $eventDate->format('M');
+           $eventDate = new DateTime(get_field('event_date'));
+           echo $eventDate-> format('M');
             ?></span>
-            <span class="event-summary__day"><?php echo $eventDate->format('j') ?></span>  
+            <span class="event-summary__day"><?php echo $eventDate->format('j');   ?></span>  
           </a>
           <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
             <p> <?php echo wp_trim_words(get_the_content(),18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
           </div>
         </div>
-         <?php } wp_reset_postdata();
+         <?php } 
         ?>
         
         
